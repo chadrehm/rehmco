ebf470e4f96d3812ffbafc0b8f8cf20e13e3c266
chad rehm <chad.rehm@kineticdata.com>
2017-12-10 12:50:42 -0600

    minor style changes

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -7,9 +7,13 @@ export const Map = ({ gmap, lat, lng, handleClick, setMap, map }) =>
   <div>
     {
       gmap ?
-      <div>
-        <GoogleSearch map={map} />
-        <GoogleMap lat={lat} lng={lng}  setMap={setMap} />
+      <div className="row">
+        <div className="col-md-4 map-legend">
+          <GoogleSearch map={map} />
+        </div>
+        <div className="col-md-8">
+          <GoogleMap lat={lat} lng={lng}  setMap={setMap} />
+        </div>
       </div>
       : <p>Loading</p>
     }
